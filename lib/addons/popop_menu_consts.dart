4d16fbf6bcfb0481f5770be855da902193cea1a9
dawn0123 <risto19941024winner@gmail.com>
2021-09-01 11:32:19 +0200

    added invoices icon

--- lib/addons/popop_menu_consts.dart
+++ lib/addons/popop_menu_consts.dart
@@ -1,12 +1,14 @@
 class Constants {
   static const String profile = 'Profile';
   static const String orders = 'Orders';
+  static const String invoices = 'Invoices';
   static const String settings = 'Settings';
   static const String signout = 'Signout';
 
   static const List<String> choices = <String>[
     profile,
     orders,
+    invoices,
     settings,
     signout
   ];
