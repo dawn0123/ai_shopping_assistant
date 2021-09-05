17dc677a2765d5cb137b2ea0ca97cc5dbc171568
dawn0123 <risto19941024winner@gmail.com>
2021-09-05 12:26:49 +0200

    save user province on db

--- test/screens/login/loginscreen_test.dart
+++ test/screens/login/loginscreen_test.dart
@@ -147,7 +147,7 @@ void main() {
     final noAccountLink = find.widgetWithText(TextLink, "Not Registered?");
 
     await tester.tap(noAccountLink);
-    await tester.pumpAndSettle();
+    await tester.pump(Duration(seconds: 2));
     expect(find.byType(RegisterScreen), findsOneWidget);
 
     print('======================');
