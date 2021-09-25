dac3bc4635ffca57e953f2d66199d3a7c6d26cc6
dawn0123 <risto19941024winner@gmail.com>
2021-09-25 12:36:45 +0200

    Decision Tree Updated

--- lib/Decision Tree Script.py
+++ lib/Decision Tree Script.py
@@ -11,25 +11,14 @@ import csv
 from IPython.display import Image
 import pydotplus
 from termcolor import colored
-
 from sklearn.model_selection import train_test_split
 import json
-
 from sklearn.tree import DecisionTreeClassifier
 from nbconvert import PythonExporter
-
 from sklearn import metrics
-from flask import Flask, jsonify
-
 import firebase_admin
 from firebase_admin import credentials,storage
 from firebase_admin import firestore
-
-import pyrebase
-
-
-# %%
-
 
 
 # %%
@@ -78,8 +67,6 @@ loadProvinceJ = json.loads(provinceJson)
    
 loadCategoryJ = json.loads(categoryJson)
 
-print(pima)
-
 
 #Here we keep track of all trained entries for later purposes...(Avoid testing the exact entries we've trained)
 # thus avoiding returning large lists as well......
@@ -88,19 +75,16 @@ with open('../assets/DecisionTreeData/Train_Data.csv','r') as file1:
 
 
 # %%
-pima
-
-
-# %%
 #Here we are collecting all our products from the database (all documents in firebase)
-print('collecting... ')
+print('collecting Products... ')
 products_ref = db.collection('Products')
 Products_docs = products_ref.get()
 
-print('collected')
-#Do the same for all the users
+print('collecting User collection... ')
+#Collecting the user's collection from firebase
 users_ref = db.collection('Users')
 users_docs = users_ref.list_documents()
+print('All set!!!')
 
 
 # %%
@@ -109,97 +93,94 @@ pima
 
 # %%
 #Declare a list of recommendations (This is one of the final products of this Algorithm)
-recommendations = []
-lists = []
+recommended_products = []
+all_products = []
 
+uid = 'joobEt92Q1O1WXlaWIcZUVdeqMf2'
 #For all users in our users' document do the following
-for doc in users_docs:
-    
-    province = ''
-    
-    #Get User information--- 
-    userDoc1 = users_ref.document(doc.id)
-    userColl1 = userDoc1.collection('info')
-    userColl2 = userDoc1.collection('Wishlist')
-    colDocs1 = userColl1.get()
-    s = False
-    colDocs2 = userColl2.list_documents()#Items in wishlist
-    info = userColl1.get()
-    
-    #
-    #    Province
-    #
-    #Load the json value
-    for colD_doc in colDocs1:
-        province = colD_doc.get('province')
-        province = loadProvinceJ[province]
 
     
-    #Note that here we're only recommending to users with a defined location, if province is null, then no recommendations
-    # the continue statement goes to the next user (iteration)... else : we iterate through every product in the database running 
-    # it through our decision tree algorithm and getting a prediction of that and that's what we'll use for recommendations.. 
-    if province == '':
-        continue
-    else:      
+province = ''
+    
+#Get User information--- 
+userDoc1 = users_ref.document(uid)
+userColl1 = userDoc1.collection('info')
+userColl2 = userDoc1.collection('Wishlist')
+colDocs1 = userColl1.get()
+s = False
+colDocs2 = userColl2.list_documents()#Items in wishlist
+info = userColl1.get()
+    
+#    Province
+#
+#Load the json value
+for colD_doc in colDocs1:
+    province = colD_doc.get('province')
+    province = loadProvinceJ[province]
+
+    
+#Note that here we're only recommending to users with a defined location, if province is null, then no recommendations
+# the continue statement goes to the next user (iteration)... else : we iterate through every product in the database running 
+# it through our decision tree algorithm and getting a prediction of that and that's what we'll use for recommendations.. 
+if province == '':
+    print("No recommendations for this user")
+else:      
         #For each product in the database :   
-        for prod_doc in Products_docs:  
-            pid = prod_doc.id  #product Id
+    for prod_doc in Products_docs:  
+        pid = prod_doc.id  #product Id
 
-            s = pid in colDocs2   #Returning true if the item is in the wishlist.....
+        s = pid in colDocs2   #Returning true if the item is in the wishlist.....
                 ###
                 # Wishlist
                 ####
-            if s :            
-                wishlist = 1
-            else:
-                wishlist = 0    
+        if s :            
+            wishlist = 1
+        else:
+            wishlist = 0    
                 
-            noOfClicks = 1    #To avoid null errors and a lot other unnecessary errors we give each product at least one click....
+        noOfClicks = 1    #To avoid null errors and a lot other unnecessary errors we give each product at least one click....
             #
             #   Clicks
             #
-            if prod_doc.get('clicks')>= 1:
-                noOfClicks = prod_doc.get('clicks')
+        if prod_doc.get('clicks')>= 1:
+            noOfClicks = prod_doc.get('clicks')
             
-            #
-            #
-            #  Product
-            #
-            category = prod_doc.get('category')
-                # print("Category: " + str(loadCategoryJ[category]))
-                # print("Clicks: " + str(noOfClicks))
-            category = loadCategoryJ[category]
+        category = prod_doc.get('category')
+        # print("Category: " + str(loadCategoryJ[category]))
+        # print("Clicks: " + str(noOfClicks))
+        category = loadCategoryJ[category]
         
-            listTemp = [pid, category, doc.id, province, 'view', noOfClicks, wishlist] #Create test entry for this product to be run through the decision tree classifier....
+        listTemp = [pid, category, uid, province, 'view', noOfClicks, wishlist] #Create test entry for this product to be run through the decision tree classifier....
 
-            if listTemp in trainedEntries : #Later purposes is now,lol, if this exact entery has been seen before in training, then don't test it
-                continue
-            else:
+        if listTemp in trainedEntries : #Later purposes is now,lol, if this exact entery has been seen before in training, then don't test it
+            continue
+        else:
                 #creating a dataframe from our entry to be able to run through the classifier...
-                test_data = pd.DataFrame([listTemp], columns=['Prod_ID', 'Prod_Cat', 'User_ID', 'User_Province', 'Event', 'Clicks', 'Wishlist']) 
+            test_data = pd.DataFrame([listTemp], columns=['Prod_ID', 'Prod_Cat', 'User_ID', 'User_Province', 'Event', 'Clicks', 'Wishlist']) 
 
-                y_predict = clf.predict(test_data[feature_cols]) #Get our 1(recommend) or 0(Do not recommend)
+            y_predict = clf.predict(test_data[feature_cols]) #Get our 1(recommend) or 0(Do not recommend)
 
-                if(y_predict == 1):
-                    str = doc.id + "|" + pid    #Using a pipe delimeter
-                    recommendations.append(str) #appending to recommendation list
-                    lists.append(str)
-                    str = colored(doc.id, 'yellow') + "|" + colored(pid, 'green')
-                    print(str)
-                    print(y_predict)
-                else:
-                    lists.append(doc.id + "|" + pid)
-                    str = colored(doc.id, 'yellow') + "|" + colored(pid, 'red')
-                    print(str)
-                    print(y_predict)
-                    
-print(len(lists))
-print(len(recommendations))
+            if(y_predict == 1):
+                str = pid    #Using a pipe delimeter
+                recommended_products.append(str) #appending to recommendation list
+                all_products.append(str)
+                str = colored(pid, 'green')
+                print(str)
+                print(y_predict)
+            else:
+                all_products.append(pid)
+                str = colored(pid, 'red')
+                print(str)
+                print(y_predict)
+
+    #Shows the total number of products to be recommended to the user compared to the original number of products...              
+    print(len(all_products))
+    print(len(recommended_products))
 
 
 # %%
 #export our final list of recommendation as csv file to our assets folder in our project....
-np.savetxt("../assets/DecisionTreeOutputs/final_recommendations.csv", recommendations, delimiter=",", fmt='%s')
+np.savetxt("../assets/DecisionTreeOutputs/final_recommendations.csv", recommended_products, delimiter=",", fmt='%s')
 
 # %% [markdown]
 # 
@@ -242,7 +223,7 @@ Image(graph.create_png())
 #Sometimes there's a tradeoff between removing redundency and a slight decrease in model accuracy... A fair tradeoff....
 # what's good about our model is that most of the time the accuracy before and after pruning is likely the same, or even better after pruning
 
-clf = DecisionTreeClassifier(criterion="entropy", max_depth=3) #Given a depth of 5 to make the tree less complicated(Avoid overfitting)
+clf = DecisionTreeClassifier(criterion="entropy", max_depth=5) #Given a depth of 5 to make the tree less complicated(Avoid overfitting)
 
 clf = clf.fit(X_train,y_train) #Train it again...
 
@@ -269,18 +250,21 @@ Image(graph.create_png())
 
 
 # %%
+#Exporting and saving our accuracy file in a form Accuracy Before Pruning, Accuracy After Pruning
 accuracy = []
 accuracy.append(AccuracyBeforePruning)
 accuracy.append(AccuracyAfterPruning)
 np.savetxt("../assets/DecisionTreeOutputs/accuracies.csv", accuracy, delimiter=",", fmt='%s')
 
-strings = []
-strings.append('DecisionTreeOutputs/final_recommendations.csv')
-strings.append('DecisionTreeOutputs/recommendations.png')
-strings.append('DecisionTreeOutputs/recommendations(Pruned).png')
-strings.append('DecisionTreeOutputs/accuracies.csv')
+#Exporting our final files to firebase (Note these are global files, files that do not depend on the user -- hence we didn't push the final _recommendations.csv file)
+#First we append them into an array...
+ai_results = []
 
-for x in strings:
+ai_results.append('DecisionTreeOutputs/recommendations.png')
+ai_results.append('DecisionTreeOutputs/recommendations(Pruned).png')
+ai_results.append('DecisionTreeOutputs/accuracies.csv')
+
+for x in ai_results:
 
     bucket = storage.bucket()
     blob = bucket.blob(x)
