2f3c87548544b36926ad5686c06db2239d2365b2
dawn0123 <risto19941024winner@gmail.com>
2021-09-27 12:48:54 +0200

    convert and save copy of recommendations.csv as .txt file

--- test/screens/signup/registerscreen_test.dart
+++ test/screens/signup/registerscreen_test.dart
@@ -77,8 +77,8 @@ void main() {
     final alreadyhaveaccount = find.widgetWithText(TextLink, 'Already have an account? Login here.');
 
     await tester.tap(alreadyhaveaccount);
-    await tester.pumpAndSettle();
-    expect(find.byType(LoginScreen), findsWidgets);
+    await tester.pump();
+    expect(find.byType(LoginScreen),findsOneWidget);
 
     print('======================');
     print('');
